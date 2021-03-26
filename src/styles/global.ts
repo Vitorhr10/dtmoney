43344d8906e00d592076e3c671ce4924f8b2ef1e
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-03-25 23:59:53 -0300

    feat: Color variables

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,16 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f2f0f5;
+    --red: #E52E4D;
+    --blue: #5429CC;
+
+    --blue-light: #6933FF;
+
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
   }
 
   * {
@@ -11,10 +21,29 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;  // 14px
+    }
+
+  }
+
   body: {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
 
-  
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
 `
